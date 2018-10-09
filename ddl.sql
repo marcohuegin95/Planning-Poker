edc16e9871a89f4b309c74ed2231c4b602f2e2af
Timon <timonmw@gmail.com>
2018-10-09 20:24:55 +0200

    Datenbankstruktur erstellt

--- ddl.sql
+++ ddl.sql
@@ -1,1 +1,146 @@
-CREATE TABLE `planningpoker`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE `email` (`email`)) ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 09. Okt 2018 um 20:22
+-- Server-Version: 10.1.34-MariaDB
+-- PHP-Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `planningpoker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rel_voting_accounts`
+--
+
+CREATE TABLE `rel_voting_accounts` (
+  `fk_account` int(11) NOT NULL,
+  `fk_voting` int(11) NOT NULL,
+  `points_voted` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rel_voting_user_sotry`
+--
+
+CREATE TABLE `rel_voting_user_sotry` (
+  `fk_user_story` int(11) NOT NULL,
+  `fk_voting` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user_story`
+--
+
+CREATE TABLE `user_story` (
+  `id` int(11) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `end` date NOT NULL,
+  `result` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `voting`
+--
+
+CREATE TABLE `voting` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indizes für die Tabelle `rel_voting_accounts`
+--
+ALTER TABLE `rel_voting_accounts`
+  ADD PRIMARY KEY (`fk_account`,`fk_voting`);
+
+--
+-- Indizes für die Tabelle `rel_voting_user_sotry`
+--
+ALTER TABLE `rel_voting_user_sotry`
+  ADD PRIMARY KEY (`fk_user_story`,`fk_voting`);
+
+--
+-- Indizes für die Tabelle `user_story`
+--
+ALTER TABLE `user_story`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `voting`
+--
+ALTER TABLE `voting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT für Tabelle `user_story`
+--
+ALTER TABLE `user_story`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `voting`
+--
+ALTER TABLE `voting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
