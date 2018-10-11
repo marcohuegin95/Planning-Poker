1f9daeb9530569295d776fd865e179aa110b1098
Timon <timonmw@gmail.com>
2018-10-11 13:05:56 +0000

    Funktion zum Speichern der User Storys

--- database/VotingDAOMySQL.php
+++ database/VotingDAOMySQL.php
@@ -21,15 +21,20 @@ class VotingDAOMySQL implements VotingDAO{
                 $new_vote_id = $this->insertIntoVotes($con, $vote);
                 $vote->setId($new_vote_id);
 
-                //TODO für jeden user insertUserRelations aufrufen
+				foreach($vote-getUsers() as $user){
+					$this->insertUserRelations($con, $vote, $user);
+				}
 
-                //TODO für jede user story eine relation anlegen
-        
+				foreach($vote->getUserStorys() as $story){
+					$this->insertUserStory($con, $vote, $story);
+				}
+               
                 $con->commit();
                 return true;
             }catch(Exception $e){
                 $con->rollback();
             }
+			return false;
                
         }
         return false;
@@ -45,6 +50,26 @@ class VotingDAOMySQL implements VotingDAO{
                 $stmt->execute(); 
     }
 
+    private function insertUserStory($con, $vote, $story){
+	    $stmt = $con->prepare("INSERT user_story (description, end) VALUES (:description, :end)");
+                $stmt->bindParam(':description', $description_var);
+                $stmt->bindParam(':end', $end_var);
+                
+                $description_var = $story->getDescription();
+                $end_var = $story->getEnd();
+                $stmt->execute(); 
+				
+		$story_id = $con->lastInsertId();
+		$story->setId($story_id);
+	
+        $stmt = $con->prepare("INSERT rel_voting_user_story (fk_user_story, fk_vote) VALUES (:storyid, :voteid)");
+                $stmt->bindParam(':storyid', $story_id_var);
+                $stmt->bindParam(':voteid', $vote_id_var);
+                
+                $vote_id_var = $vote->getId();
+                $story_id_var = $story->getId();
+                $stmt->execute(); 
+    }
     
 
     private function insertIntoVotes($con, $vote){
