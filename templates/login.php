ffa107db359e435d2e4e91066d00ada64a4d59a3
Marco Hügin <hueginma@dhbw-loerrach.de>
2018-10-18 23:24:22 +0200

    Login-Seite und Einstellungsseite angepasst

--- templates/login.php
+++ templates/login.php
@@ -1,27 +1,54 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="templates/css/login.css">
+    <title>Account-Einstellungen</title>
+    <h1 class="text-hide" style="background-image: url('/images/karte.png); width: 50px; height: 50px;">Bootstrap</h1>
+    <link rel="stylesheet" href="css/bewertung.css">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/index.css">
 
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Planning Poker</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mr-auto">
+
+            </div>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        timonmw@gmail.com
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Log out</a>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="jumbotron">
+    <?php echo $this->err;
+          echo $this->message;  
+                            ?>
     <div class="container">
         <div id="loginFrame" class="row">
             <div class="col-md-4"></div>
-
             <div class="col-md-4">
-                <?php echo $this->err;
-                      echo $this->message;  
-                ?>
-                
                 <div class="card">
                     <div class="card-header">
                         <h4>Scrum Planning Poker</h4>
@@ -30,24 +57,23 @@
                                 <a class="nav-link active" data-toggle="tab" href="#login">Login</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="#register">Register</a>
+                                <a class="nav-link" data-toggle="tab" href="#register">Registrieren</a>
                             </li>
                         </ul>
 
                     </div>
                     <div class="card-body">
-
                         <div class="tab-content">
                             <div id="login" class="tab-pane active">
                                 <form action="/Login" method="post">
                                     <div class="form-group">
-                                        <label for="inputEmail">Email address</label>
+                                        <label for="inputEmail">E-Mail</label>
                                         <input name="email" type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter email" required>
+                                            placeholder="E-Mail eingeben" required>
                                     </div>
                                     <div class="form-group">
-                                        <label for="inputPassword">Password</label>
-                                        <input name="password" type="password" class="form-control" id="loginPassword" placeholder="Password" required>
+                                        <label for="inputPassword">Passwort</label>
+                                        <input name="password" type="password" class="form-control" id="loginPassword" placeholder="Passwort" required>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Login</button>
                                 </form>
@@ -55,40 +81,50 @@
 
                             <div id="register" class="tab-pane">
                                 <form action="/Register" method="post">
+                                <div class="form-group">
+                                        <label for="inputUser">Vorname</label>
+                                        <input name="vorname" type="test" class="form-control" id="registertVorname" placeholder="Vorname eingeben" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="inputUser">Nachname</label>
+                                        <input name="nachname" type="test" class="form-control" id="registertNachname" placeholder="Nachname eingeben" required>
+                                        </div>
+                                        <div class="form-group">
+                                    <label for="sel1">Erfahrung des Mitarbeiter</label>
+                                    <select class="form-control" id="sel1">
+                                        <option>Gar keine</option>
+                                        <option>Wenig</option>
+                                        <option>Einige</option>
+                                        <option>Viel</option>
+                                    </select>
+                                    </div>                               
                                     <div class="form-group">
                                         <label for="inputUser">Username</label>
-                                        <input name="username" type="test" class="form-control" id="registertUser" placeholder="Enter username" required>
+                                        <input name="username" type="test" class="form-control" id="registertUser" placeholder="Username eingeben" required>
                                     </div>
                                     <div class="form-group">
-                                        <label for="inputEmail">Email address</label>
+                                        <label for="inputEmail">E-Mail Adresse</label>
                                         <input name="email" type="email" class="form-control" id="registerEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter email" required>
+                                            placeholder="E-Mail eingeben" required>
                                     </div>
                                     <div class="form-group">
-                                        <label for="inputPassword">Password</label>
-                                        <input name="password" type="password" class="form-control" id="registerPassword" placeholder="Password" required>
+                                        <label for="inputPassword">Passwort</label>
+                                        <input name="password" type="password" class="form-control" id="registerPassword" placeholder="Passwort eingeben" required>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Register</button>
                                 </form>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
-
-
             </div>
-
             <div class="col-md-4"></div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
+    </div>
+    <script src="js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
 
 </body>
 
