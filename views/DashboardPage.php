97317e4cd3275c6aa25d75b5afc0209f59b7aed4
Timon <timonmw@gmail.com>
2018-10-29 21:51:55 +0100

    Anpassung Dashboard

--- views/DashboardPage.php
+++ views/DashboardPage.php
@@ -78,7 +78,6 @@ class DashboardPage extends MessagePage implements Page{
                             <div class="list-group-item list-group-item-action flex-column align-items-start active">
                                 <div class="d-flex w-100 justify-content-between">
                                     <h5 class="mb-1">'.$oldgame->getName().'
-                                        <span class="badge badge-success">45SP</span>
                                     </h5>
                                     <small>'.$oldgame->getEnd().'</small>
                                 </div>
